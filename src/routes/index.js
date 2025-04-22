526d567d27890ef6e509c725d0f174c7ae8c84d3
Raul <rauloliveira08@gmail.com>
2025-04-22 16:09:24 -0300

    atualização

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,17 @@
- 
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Retorna uma mensagem de boas-vindas
+ *     responses:
+ *       200:
+ *         description: Mensagem de boas-vindas
+ */
+router.get('/', (req, res) => {
+  res.json({ mensagem: 'API candido' });
+});
+
+module.exports = router;
